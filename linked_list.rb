6f98f2d57e822b1d66230a18dd7362574c83a1a1
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-27 16:52:39 -0500

    adds functionality to: #tail #prepend #head #contains?

--- linked_list.rb
+++ linked_list.rb
@@ -1,36 +1,55 @@
 class LinkedList
-  def initialize; end
+  def initialize(data)
+    @head = Node.new(data)
+  end
 
-  def append; end
+  def append(data); end
 
-  def size; end
+  def prepend(data)
+    n = Node.new(data)
+    n.node = @head
+    @head = n
+  end
 
-  def head; end
+  def size
+    # iterate through the nodes and count amount of iterations
+  end
 
-  def tail; end
+  def tail
+    current_node = @head
+    current_node = current_node.next_node while current_node.next_node
+    current_node
+  end
+
+  def head
+    @head.data
+  end
 
   def at(index); end
 
   def pop; end
 
-  def contains?; end
+  def contains?(value)
+    current_node = @head
+    while current_node
+      return true if current_node.data == value
+      current_node = current_node.next_node
+    end
+  end
 
   def find(data); end
 
   def to_s; end
 
-  def insert_at(index); end
+  def insert_at(index); end # extra credit
 
-  def remove_at(index); end
+  def remove_at(index); end # extra credit
 
   class Node
-    def initialize
-      @value = nil
+    attr_accessor :data, :node
+    def initialize(data = nil)
+      @data = data
       @next_node = nil
     end
-
-    def value; end
-
-    def next_node; end
   end
 end
